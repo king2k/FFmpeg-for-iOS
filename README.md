04c92e4c47f61f8a816e466d7dbb0f4d1241aafa
king2k <bluecq@gmail.com>
2014-03-07 21:43:55 +0800

    support ios7 and arm64

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 you can modify  the script to enable the encoder \decoder\muxer\demuxer as you need
 
-now use ffmpeg 1.1
+now use ffmpeg 2.1.4
+
+support ios7 arm64
 
 oops,you should download gas-preprocessor from https://github.com/mansr/gas-preprocessor, and copy it to your /usr/sbin 
--- build-ffmpeg-only.sh
+++ build-ffmpeg-only.sh
@@ -6,8 +6,8 @@
 ###########################################################################
 #  Change values here													  #
 #																		  #
-VERSION="1.1"													      #
-SDKVERSION="6.0"														  #
+VERSION="2.1.4"													      #
+SDKVERSION="7.0"														  #
 #																		  #
 ###########################################################################
 #																		  #
@@ -17,7 +17,7 @@ SDKVERSION="6.0"														  #
 
 
 CURRENTPATH=`pwd`
-ARCHS="i386 armv7 armv7s"
+ARCHS="i386 armv7 armv7s arm64"
 DEVELOPER=`xcode-select -print-path`
 
 if [ ! -d "$DEVELOPER" ]; then
@@ -43,7 +43,11 @@ mkdir -p "${CURRENTPATH}/lib"
 
 tar jxvf ffmpeg-${VERSION}.tar.bz2 -C "${CURRENTPATH}/src"
 cd "${CURRENTPATH}/src/ffmpeg-${VERSION}"
-
+#sed -e 's/enabled librtmp/#enabled librtmp/' configure > configure.new
+#mv configure configure.old
+#mv configure.new configure
+#chmod +x configure
+ 
 for ARCH in ${ARCHS}
 do
 	if [ "${ARCH}" == "i386" ];
@@ -52,12 +56,24 @@ do
 		EXTRA_FLAGS="--cpu=i386 --enable-pic --disable-yasm --disable-asm --disable-armv5te"
         EXTRA_CFLAGS=""
         FFARCH=i386
+        OSVER="-miphoneos-version-min=5.0.0"
+        DISABLEASM=""
+    elif [ "${ARCH}" == "arm64" ]
+	then
+		PLATFORM="iPhoneOS"
+		EXTRA_FLAGS=""
+        EXTRA_CFLAGS="-mfpu=neon"
+        FFARCH=arm
+		OSVER="-miphoneos-version-min=7.0.0"
+		DISABLEASM="--disable-asm"
 	else
 		#sed -ie "s!static volatile sig_atomic_t intr_signal;!static volatile intr_signal;!" "crypto/ui/ui_openssl.c"
 		PLATFORM="iPhoneOS"
 		EXTRA_FLAGS="--cpu=cortex-a8 --enable-pic"
         EXTRA_CFLAGS="-mfpu=neon"
         FFARCH=arm
+        OSVER="-miphoneos-version-min=5.0.0"
+        DISABLEASM=""
 	fi
 	
 	export CROSS_TOP="${DEVELOPER}/Platforms/${PLATFORM}.platform/Developer"
@@ -67,12 +83,13 @@ do
 	echo "Building ffmpeg-${VERSION} for ${PLATFORM} ${SDKVERSION} ${ARCH}"
 	echo "Please stand by..."
 
+	#export CC="${CROSS_TOP}/usr/bin/gcc -arch ${ARCH}"
 	mkdir -p "${CURRENTPATH}/bin/${PLATFORM}${SDKVERSION}-${ARCH}.sdk"
 	LOG="${CURRENTPATH}/bin/${PLATFORM}${SDKVERSION}-${ARCH}.sdk/build-ffmpeg-${VERSION}.log"
 
 ./configure \
---cc=${CROSS_TOP}/usr/bin/gcc \
---as="gas-preprocessor.pl ${CROSS_TOP}/usr/bin/gcc" \
+--cc="${DEVELOPER}/Toolchains/XcodeDefault.xctoolchain/usr/bin/clang ${OSVER} -v" \
+--as="gas-preprocessor.pl ${DEVELOPER}/Toolchains/XcodeDefault.xctoolchain/usr/bin/clang" \
 --prefix=${CURRENTPATH}/bin/${PLATFORM}${SDKVERSION}-${ARCH}.sdk \
 --sysroot=${CROSS_TOP}/SDKs/${CROSS_SDK} \
 --extra-cflags="-I../../include" \
@@ -85,21 +102,26 @@ do
 --extra-ldflags="-arch ${ARCH}" \
 ${EXTRA_FLAGS} \
 --enable-gpl \
+${DISABLEASM} \
 --enable-cross-compile \
 --disable-ffmpeg  \
 --disable-ffplay \
 --disable-ffserver \
 --disable-doc \
---disable-encoders \
---disable-decoders \
---disable-demuxers \
---disable-muxers \
---enable-demuxer=flv \
---enable-demuxer=mov \
---enable-muxer=flv \
---enable-muxer=mov \
---enable-decoder=mp3 \
---enable-decoder=aac \
+#--enable-libx264 \
+#--enable-encoder=libx264 \
+#--enable-encoder=libx264rgb \
+#--enable-libmp3lame \
+#--enable-encoder=libmp3lame \
+#--enable-libfdk-aac \
+#--enable-encoder=libfdk_aac \
+#--enable-nonfree \
+#--enable-librtmp \
+#--extra-ldflags="-lssl -lrtmp -lcrypto -lz" \
+
+#--disable-encoders \
+#--disable-decoders \
+#--disable-demuxers \
 
 	make >> "${LOG}" 2>&1
 	make install >> "${LOG}" 2>&1
@@ -110,7 +132,7 @@ echo "Build library..."
 FFMPEG_LIBS="libavcodec libavdevice libavformat libavutil libswscale"
 for i in ${FFMPEG_LIBS}
 do 
-	lipo -create ${CURRENTPATH}/bin/iPhoneSimulator${SDKVERSION}-i386.sdk/lib/$i.a  ${CURRENTPATH}/bin/iPhoneOS${SDKVERSION}-armv7.sdk/lib/$i.a ${CURRENTPATH}/bin/iPhoneOS${SDKVERSION}-armv7s.sdk/lib/$i.a -output ${CURRENTPATH}/lib/$i.a
+	lipo -create ${CURRENTPATH}/bin/iPhoneSimulator${SDKVERSION}-i386.sdk/lib/$i.a  ${CURRENTPATH}/bin/iPhoneOS${SDKVERSION}-armv7.sdk/lib/$i.a ${CURRENTPATH}/bin/iPhoneOS${SDKVERSION}-armv7s.sdk/lib/$i.a ${CURRENTPATH}/bin/iPhoneOS${SDKVERSION}-arm64.sdk/lib/$i.a -output ${CURRENTPATH}/lib/$i.a
 done
 
 mkdir -p ${CURRENTPATH}/include
